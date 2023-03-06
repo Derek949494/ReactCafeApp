cf54d6b0937c5645e986f18fd0bb4d3afe9e6c32
Derek <derek.snyder94@gmail.com>
2023-03-06 15:53:17 -0800

    hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Button, Nav,NavLink} from 'reactstrap';
 import './NavbarStyles.css';
 import {Link} from 'react-router-dom';
@@ -7,35 +7,46 @@ import logo from '../assets/CafeUpdateImages/CafeLogoSmall.jpg'
 
 
 const Navbar = () => {
+
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => {
+    setClick(!click)
+  }
+
+
   return (
     <div className='header'>
         <div className='container'>
             <div className='nav-bar'>
                 <Link to='/'>
-                <img src={logo} alt='cafe-logo' width={100} />
+                <img src={logo} alt='cafe-logo' width={90} />
                 </Link>
 
-                <ul className='nav-menu'>
-                    <li>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li onClick={handleClick}>
                         <NavLink className='nav-link' to='/'>Home</NavLink>
                     </li>
-                    <li>
+                    <li onClick={handleClick}>
                         <NavLink className='nav-link' to='/about'>About</NavLink>
                     </li>
-                    <li>
+                    <li onClick={handleClick}>
                         <NavLink className='nav-link' to= '/menu'>Menu</NavLink>
                     </li>
-                    <li>
+                    <li onClick={handleClick}>
                         <NavLink className='nav-link' to='/contact'>Contact</NavLink>
                     </li>
 
-                    <li>
+                    <li onClick={handleClick}>
                         <Button>Order Now</Button>
                     </li>
                 </ul>
-                <div className='hamburger'>
-                    <FaTimes size={20} style={{color: "#305252"}}/>
+                <div className='hamburger' onClick={handleClick}>
+                    { click ? (
+                        <FaTimes size={20} style={{color: "#305252"}}/>
+                        ) : (
                     <FaBars size={20} style={{color: "#305252"}}/>
+                    )}
                 </div>
             </div>
         </div>
