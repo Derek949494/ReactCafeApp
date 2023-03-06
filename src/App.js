0473f583ee919cf1f6a1929f0ca8f18e58c05c03
Derek <derek.snyder94@gmail.com>
2023-03-06 14:59:54 -0800

    navbar changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import MenuPage from './pages/MenuPage';
+import Footer from './components/Footer';
+
+
 
 
 function App() {
   return (
       <div className='App'>
         <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+          <Route path='/menu' element={<MenuPage />} />
+        </Routes>
+        <Footer />
       </div>
   );
 }
