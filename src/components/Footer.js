88ec3e8ce97cf7cc9aa56cea5b07b0ad742cd3fd
Derek <derek.snyder94@gmail.com>
2023-03-08 10:19:38 -0800

    save before massive changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,95 @@
 
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="site-footer">
+        <Container>
+            <Row>
+                <Col xs={{ size: 4, offset: 1 }} sm='2'>
+                    <h5>Links</h5>
+                    <ul className='list-unstyled'>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/about'>About</Link>
+                        </li>
+                        <li>
+                            <Link to='/menu'>Menu</Link>
+                        </li>
+                        <li>
+                            <Link to='/contact'>Contact</Link>
+                        </li>
+                    </ul>
+                </Col>
+                <Col xs='6' sm='3' className='text-center'>
+                    <h5>Social</h5>
+                        <a
+                            className='btn btn-social-icon btn-instagram'
+                            href='http://instagram.com/'
+                        >
+                            <i className='fa fa-instagram' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-facebook'
+                            href='http://www.facebook.com/'
+                        >
+                            <i className='fa fa-facebook' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-twitter'
+                            href='http://twitter.com/'
+                        >
+                            <i className='fa fa-twitter' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-google'
+                            href='http://youtube.com/'
+                        >
+                            <i className='fa fa-youtube' />
+                        </a>
+                </Col>
+                <Col xs='6' sm='3' className='text-center'>
+                    <a
+                            role='button'
+                            className='btn btn-link'
+                            href='tel:+12065551234'
+                        >
+                            <i className='fa fa-phone' /> 1-222-333-4444
+                        </a>
+                        <br />
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='mailto:notreal@notreal.co'
+                        >
+                            <i className='fa fa-envelope-o' /> Dereks@cafe.co
+                        </a>
+                </Col>
+                <Col sm='4' className='text-center'>
+                <a
+                            role='button'
+                            className='btn btn-link'
+                            href='tel:+12065551234'
+                        >
+                            <i className='fa fa-phone' /> 1-222-333-4444
+                        </a>
+                        <br />
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='mailto:notreal@notreal.co'
+                        >
+                            <i className='fa fa-envelope-o' /> Dereks@cafe.co
+                        </a>
+                </Col>
+            </Row>
+        </Container>
+
+    </footer>
   )
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import {Link}  from 'react-router-dom';
-import BannerImage from '../assets/homepage1.jpeg';
-import './HomePageStyles.css';
 
 const HomePage = () => {
   return (
-    <div className='home'>
-      <div className='headerContainer' style={{ backgroundImage: `url(${BannerImage})` }}>
-        <h1>Derek's Cafe</h1>
-        <p>Tacoma's Best Coffee</p>
-        <Link to='/menu'>
-        <button> Menu</button>
-        </Link>
-      </div>
+    <div>
+    
     </div>
   )
 }
